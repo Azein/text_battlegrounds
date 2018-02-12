d089c85f9a219ccec21e31e568242168eeb7faf1
Максим Селютин <azein@Maksims-MacBook-Pro.local>
2018-02-12 18:04:08 +0300

    started on character creation

--- src/screens/CharCreation/ducks.js
+++ src/screens/CharCreation/ducks.js
@@ -1,5 +1,68 @@
-const charCreationReducer = () => ({
-  dd: 'dddd',
+// @flow
+
+import type { charStats } from 'types/character'
+
+const INCREASE_STAT = '@BATTLEGROUNDS/INCREASE_STAT'
+const DECREASE_STAT = '@BATTLEGROUNDS/DECREASE_STAT'
+
+type State = {
+  ...charStats,
+  freePoints: number,
+  rules: {
+    statMax: number,
+  },
+}
+
+type Payload = {
+  name?: string,
+  amount?: number,
+}
+
+type Action = {
+  type: string,
+  payload: Payload,
+}
+
+type ChangeStat = (string, number) => Action
+
+const inreaseStat: ChangeStat = (name, amount) => ({
+  type: INCREASE_STAT,
+  payload: {
+    name,
+    amount,
+  },
 })
+
+const decreaseStat: ChangeStat = (name, amount) => ({
+  type: DECREASE_STAT,
+  payload: {
+    name,
+    amount,
+  },
+})
+
+const initialState = {
+  stats: {
+    aim: 0,
+    awareness: 0,
+    stealth: 0,
+    reaction: 0,
+    transport: 0,
+  },
+  freePoints: 10,
+  rules: {
+    statMax: 5,
+  },
+}
+
+const charCreationReducer = (
+  state: State = initialState,
+  { type, payload }: Action,
+) => {
+  switch (type) {
+    default:
+      return state
+  }
+}
 
 export default charCreationReducer
---
+++ src/types/character.js
@@ -0,0 +1,9 @@
+// @flow
+
+export type charStats = {
+  aim: number,
+  awareness: number,
+  stealth: number,
+  reaction: number,
+  transport: number,
+}
