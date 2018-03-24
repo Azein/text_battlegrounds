456a9f2488de6ece9e77165f4d1d6f0496dd812b
Azein <azeinpwr@gmail.com>
2018-03-25 01:49:23 +0300

    basic layout for char creation

--- src/screens/CharCreation/index.js
+++ src/screens/CharCreation/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Header from 'screens/parts/header'
-import { StyledButton } from './styled'
+import { AnimatedButton } from 'ui/components/Button'
+import {
+  StyledButton,
+  Layout,
+  HorizontalLayout,
+  StatsSection,
+  DescSection,
+  StartSection,
+  Description,
+} from './styled'
 
 const CharCreation = () => (
-  <div>
+  <Layout>
     <Header />
-    <StyledButton>YOLO</StyledButton>
-  </div>
+    <HorizontalLayout>
+      <StatsSection />
+      <DescSection>
+        <Description />
+        <StartSection>
+          <AnimatedButton>Начать игру</AnimatedButton>
+        </StartSection>
+      </DescSection>
+    </HorizontalLayout>
+  </Layout>
 )
 
 export default CharCreation
--- src/screens/CharCreation/styled.js
+++ src/screens/CharCreation/styled.js
@@ -1,8 +1,47 @@
 import styled from 'styled-components'
 import Button from '@kupibilet/ui/components/Button'
+import { Layout } from 'ui/components/Layout'
 
 const StyledButton = styled(Button)`
   border-radius: 0;
 `
 
-export { StyledButton }
+const HorizontalLayout = styled(Layout)`
+  flex-direction: row;
+  flex-flow: nowrap;
+`
+
+const StatsSection = styled(Layout)`
+  width: 50%;
+  background-color: ${({ theme }) => theme.color.darkestGrey};
+`
+
+const DescSection = styled(Layout)`
+  width: 50%;
+  background-color: ${({ theme }) => theme.color.darkerGrey};
+`
+
+const StartSection = styled(Layout)`
+  flex-direction: row;
+  flex-grow: 0;
+  flex-shrink: 0;
+  height: 80px;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 10px;
+  padding-right: 40px;
+`
+
+const Description = styled(Layout)`
+  color: ${({ theme }) => theme.color.textWhite};
+`
+
+export {
+  StyledButton,
+  Layout,
+  HorizontalLayout,
+  StatsSection,
+  DescSection,
+  StartSection,
+  Description,
+}
